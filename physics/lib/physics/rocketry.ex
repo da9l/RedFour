6538621399ff9831dda617837d4da62ca2f5899b
Daniel Hegner <daniel@hegner.se>
2016-07-02 11:20:14 +0200

    Started on orbital radius

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -1,41 +1,12 @@
-defmodule Converter do
-  def to_nearest_tenth(val) do
-    Float.ceil val, 1
-  end
-
-  def to_km(velocity) do
-    velocity / 1000
-  end
-
-
-  def to_light_seconds({:miles, miles}) do
-    (miles *5.36819e-6) |> round_down
-  end
-
-  def to_light_seconds({:meters, meters}) do
-    (meters *3.335638620368e-9) |> round_down
-  end
-
-  def round_down(val) when is_float(val), do: trunc(val) 
-end
-
 defmodule Physics.Rocketry do
-
+  import Converter
+  import Calcs
+  import Physics.Laws
+  import Planets
   
-  def escape_velocity(:earth) do
-    %{mass: 5.972e24, radius: 6.371e6}
-      |> escape_velocity
-  end
-
-  def escape_velocity(:mars) do
-    %{mass: 6.39e23, radius: 3.4e6}
-      |> escape_velocity
-  end
-
-  def escape_velocity(:moon) do
-    %{mass: 7.35e22, radius: 1.738e6}
-      |> escape_velocity
-  end
+  def escape_velocity(:earth), do: earth |> escape_velocity
+  def escape_velocity(:mars), do: mars |> escape_velocity
+  def escape_velocity(:moon), do: moon |> escape_velocity
 
   def escape_velocity(planet) when is_map(planet) do
     planet 
@@ -44,9 +15,16 @@ defmodule Physics.Rocketry do
       |> Converter.to_nearest_tenth
   end
 
+  def orbital_speed(height) do
+    newtons_
+  end
+
+  defp orbital_radius(height) do
+    earth.radius + (height |> to_meters)
+  end
+#1:00:43
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    newtons_constant = 6.67e-11
-    2 * newtons_constant * mass / radius
-      |> :math.sqrt
+    2 * newtons_gravitational_constant * mass / radius
+      |> square_root
   end
 end
